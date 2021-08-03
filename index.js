cb253cb3d24b8b40a6292eaaedc4719a24ee8ea0
SantishaR <82038785+SantishaR@users.noreply.github.com>
2021-08-03 15:11:16 +0200

    Added content to index

--- index.js
+++ index.js
@@ -3,29 +3,50 @@ export const HomePage = (() => {
     // creating DOM elements
     
     const homeSection = document.createElement('div');
-    const heading = document.createElement('h1');
-    const paragraph = document.createElement('p');
-    const headingTwo = document.createElement('h2');
+    const mainHeading = document.createElement('h1');
+    const slogan = document.createElement('p');
+
+    const addressHead = document.createElement('h2');
+    const address = document.createElement('p');
+
+    const contactUs = document.createElement('h2');
     const email = document.createElement('p');
     const number = document.createElement('p');
 
+    const hours = document.createElement('h2');
+    const times = document.createElement('p');
+    const timesTwo = document.createElement('p');
 
+// adding page content
     homeSection.classList.add('content-section');
     
-    heading.textContent = 'Sky Dining';
-    paragraph.textContent = 'Fine dining at its highest!';
-    headingTwo.textContent = 'Contact Us';
+    mainHeading.textContent = 'Sky Dining';
+    slogan.textContent = 'Fine dining at its highest!';
+
+    addressHead.textContent = 'Address';
+    address.textContent = 'Magwa Crescent, Waterfall City, Midrand';
+
+    contactUs.textContent = 'Contact Us';
     email.textContent = 'info@skydining.co.za';
     number.textContent = '011-254-8545';
+
+    hours.textContent = 'Opening Hours';
+    times.textContent = 'Mon-Fri: 9am - 8pm';
+    timesTwo.textContent = 'Sat- Sun: 9am - 10pm';
 
 
 // append elements to parent div
     
-homeSection.appendChild(heading);
-homeSection.appendChild(paragraph);
-homeSection.appendChild(headingTwo);
+homeSection.appendChild(mainHeading);
+homeSection.appendChild(slogan);
+homeSection.appendChild(contactUs);
 homeSection.appendChild(email);
 homeSection.appendChild(number);
+homeSection.appendChild(hours);
+homeSection.appendChild(times);
+homeSection.appendChild(timesTwo);
+homeSection.appendChild(addressHead);
+homeSection.appendChild(address);
 
    
     return {homeSection}
--- main.js
+++ main.js
@@ -5,13 +5,13 @@ import { MenuPage } from './menu.js';
 const indexBody = document.querySelector('.index-body');
 const menuBody = document.querySelector('.menu-body');
 const container = document.querySelector('#container');
-const containerOne = document.querySelector('#menu-container');
+const menuContainer = document.querySelector('#menu-container');
 
  document.body.appendChild(Head.navbar);
 container.appendChild(HomePage.homeSection);
 indexBody.appendChild(container);
 
-containerOne.appendChild(MenuPage.menuBox);
-menuBody.appendChild(containerOne);
+menuContainer.appendChild(MenuPage.menuBox);
+menuBody.appendChild(menuContainer);
 
 
