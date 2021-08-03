e01124c4788b28548785d68f087de61120daea13
SantishaR <82038785+SantishaR@users.noreply.github.com>
2021-08-03 18:06:04 +0200

    fixed main.js

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const container = document.querySelector('#container');
 const menuContainer = document.querySelector('#menu-container');
 
  document.body.appendChild(Head.navbar);
-container.appendChild(HomePage.homeSection);
-indexBody.appendChild(container);
+container?.appendChild(HomePage.homeSection);
+indexBody?.appendChild(container);
 
-menuContainer.appendChild(MenuPage.menuBox);
-menuBody.appendChild(menuContainer);
+menuContainer?.appendChild(MenuPage.menuBox);
+menuBody?.appendChild(menuContainer);
 
 
